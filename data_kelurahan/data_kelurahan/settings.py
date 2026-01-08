6ca22f13d2da5beeb649cba88c010ae2246765f2
tegarbagaskara <tegarbagasakara.com>
2026-01-08 08:25:36 +0700

    Pertemuan 12: Implementasi Dokumentasi API Swagger

--- data_kelurahan/data_kelurahan/settings.py
+++ data_kelurahan/data_kelurahan/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
@@ -141,6 +143,13 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'API Aplikasi Warga Kelurahan',
+    'DESCRIPTION': 'Dokumentasi API untuk mengelola data warga dan pengaduan.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 #CORS_ALLOWED_ORIGINS=[
 #    "http://localhost:4500",
--- data_kelurahan/data_kelurahan/urls.py
+++ data_kelurahan/data_kelurahan/urls.py
@@ -19,6 +19,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('', include('warga.urls')), 
@@ -26,4 +27,16 @@ urlpatterns = [
     path('api/', include('warga.api_urls')), 
     path('api/auth/token/', obtain_auth_token, name='api-token-auth'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # --- URL DOKUMENTASI API ---
+    # Menghasilkan file schema.yml
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Halaman Swagger UI
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Halaman Redoc
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+    
+
+
